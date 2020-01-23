9862f78ffd899f5ec4c9099d725e9185cb71f062
JeffreyYou55 <59158990+JeffreyYou55@users.noreply.github.com>
2020-01-23 12:00:07 +0900

    200123

---
+++ 200123/structure.py
@@ -0,0 +1,15 @@
+print ({"name" : "Lucian",
+"married" : True,
+"isJungle" : False,
+"Ult" : 12,
+"Passive" : "2 shots after skills , second shot can be critical"} )
+print ({"name" : "Lucian",
+"married" : True,
+"isJungle" : False,
+"Ult" : 12,
+"Passive" : "2 shots after skills , second shot can be critical"}["Passive"] )
+print ({"name" : "Lucian",
+"married" : True,
+"isJungle" : False,
+"Ult" : 12,
+"Passive" : "2 shots after skills , second shot can be critical"}["Ult"] * 14.3 )
