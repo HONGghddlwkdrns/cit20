b5ad560b24fc0d969378d8d04fd1eafbb3d0d1eb
HONGghddlwkdrns <59945379+HONGghddlwkdrns@users.noreply.github.com>
2020-02-13 11:59:47 +0900

    ddd

---
+++ 200210/369.py
@@ -0,0 +1,12 @@
+num = 1
+divider = 3
+remainder = 0
+C = "clap"
+
+
+while(num < 20):
+    if (num % divider != remainder):
+        print(num)
+    else :
+        print(C)
+            num = num + 1
---
+++ 200213/var2.py
@@ -0,0 +1,10 @@
+color=[ "red", "blue" , "green" ]
+print( color [2])
+color[2] = "black"
+print (color)
+lucian = { "atk" : 75 , "chrome" : "gold"}
+print (lucian)
+color[1] = lucian["chrome"]
+lucian["chrome"] = color[1]
+print (color)
+print (lucian)
